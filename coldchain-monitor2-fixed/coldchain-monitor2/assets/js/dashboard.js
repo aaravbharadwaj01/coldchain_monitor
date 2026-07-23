d78a558a0ce45d317bbb497def2c680ac2ee12b8
aaravbharadwaj01 <aaravbharadwaj01@gmail.com>
2026-07-24 02:18:02 +0530

    Update dashboard.js

--- coldchain-monitor2-fixed/coldchain-monitor2/assets/js/dashboard.js
+++ coldchain-monitor2-fixed/coldchain-monitor2/assets/js/dashboard.js
@@ -628,7 +628,7 @@ async function loadDashboardData() {
     const banner = document.getElementById("thresholdAlertBanner");
 
     try {
-        const response = await fetch("http://localhost:6060/api/latest");
+        const response = await fetch("https://coldchain-monitor.onrender.com/api/latest");
 
         if (!response.ok) {
             throw new Error("Failed to fetch sensor data");
@@ -750,7 +750,7 @@ async function loadAlertsFromAPI(){
     try {
 
         const response = await fetch(
-            "http://localhost:6060/api/alerts"
+            "https://coldchain-monitor.onrender.com/api/alerts"
         );
 
         const alerts = await response.json();
