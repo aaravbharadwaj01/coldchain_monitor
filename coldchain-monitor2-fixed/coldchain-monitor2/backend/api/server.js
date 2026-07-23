ff3fdb3dbd79ee5f13f1d39ae458eeab3706a0d8
aaravbharadwaj01 <aaravbharadwaj01@gmail.com>
2026-07-23 10:52:33 +0530

    Update server.js

--- coldchain-monitor2-fixed/coldchain-monitor2/backend/api/server.js
+++ coldchain-monitor2-fixed/coldchain-monitor2/backend/api/server.js
@@ -37,7 +37,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "JFEuxsgKDNWGhdoFSzDJzOkHqYrDGLOn",
   database: process.env.DB_NAME || "coldchain_monitor",
   waitForConnections: true,
   connectionLimit: 10
